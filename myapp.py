80601362591da9f6ba136bc3c93a52d60be5e367
Ian-Harland <iharland@udel.edu>
2018-05-09 17:54:47 -0400

    flask II

---
+++ myapp.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/users/<string:username>')
+def users(username):
+    return render_template('users.html', username=username)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+if __name__ == '__main__':
+    app.run()
